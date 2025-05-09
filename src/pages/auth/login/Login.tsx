b2ec2f49761c58010f1e1e19176215e7eed21536
PriteshUE <support@userex.in>
2025-05-09 12:30:45 +0530

    initial folder structure

---
+++ src/pages/auth/login/Login.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+function Login() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <Button>Login</Button>
+      </div>
+    </>
+  );
+}
+
+export default Login;
